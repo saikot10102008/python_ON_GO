92f96ba593a1d2da6789fc0001697e960de10d37
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2025-12-27 01:28:33 +0600

    membership operator

---
+++ py15.py
@@ -0,0 +1,13 @@
+"""
+Membership Operators in Python
+Membership operators are used to test whether a value or variable exists within a sequence (like strings, lists, tuples, sets, or dictionaries).
+
+Two Membership Operators:
+in - Returns True if a value is found in the sequence
+
+not in - Returns True if a value is NOT found in the sequence
+"""
+str_a = "a string"
+print("a" in str_a)
+print("string" in str_a)
+print("b" in str_a)
