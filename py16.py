2eb1e1a54ff13850ff254d5b003b11b082fbc927
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2026-04-06 23:48:10 +0600

    string slicing experimentation

---
+++ py16.py
@@ -0,0 +1,25 @@
+# string and string slicing experimentation
+
+
+string_var = "STRING"
+
+a = string_var[-1]
+b = string_var[5]
+
+print(a,b) # comma ( , ) adds a space btween a and b variable
+
+print(string_var[0:5:1]) # STRIN
+print(string_var[:5:1]) # STRIN
+print(string_var[0::1]) # STRING
+print(string_var[0:5:]) # STRIN
+print(string_var[0:6:]) # STRING
+print(string_var[0:999999999999999999999999999999999999999999999999999999999:]) # STRING
+print(string_var[0:5]) # STRIN
+print(string_var[::1]) # STRING
+print(string_var[::2]) # SRN
+print(string_var[::]) # STRING
+
+
+
+
+
