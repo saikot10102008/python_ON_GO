fcb7b132ac7d42c28d63f303b5e5c3ea08765568
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2026-04-06 23:51:46 +0600

    string slicing and , and +

--- py16.py
+++ py16.py
@@ -19,6 +19,9 @@ print(string_var[::1]) # STRING
 print(string_var[::2]) # SRN
 print(string_var[::]) # STRING
 
+print (" Type of this data type: ",type(string_var))
+print(" we can use comma to concatenate anything but we cannot we + unless until both are string")
+print("string-1"+" string-2") # comma provides a space but + doesn't
 
 
 
