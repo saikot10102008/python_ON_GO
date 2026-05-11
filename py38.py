7333d44d0e5e378f718c4bf7f5c66f48e9d14396
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2026-05-12 02:32:46 +0600

    list-2

---
+++ py38.py
@@ -0,0 +1,12 @@
+a = [1,2,3,4,5,6,7,8,9]
+
+# len(a) gives the number of elements inside which is 9 in case of [a]
+
+
+for i in range(len(a)): # here len(a) is 9 and range(len(a)) means range(0:9:1)
+    print(a[i])
+
+# directly access list
+print("\n")
+for i in a:
+    print(i)
